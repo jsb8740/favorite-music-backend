720775d1a170deb3d46620b1f0cc9c81767378da
qwer1234 <qwer1234@jinjoosoft.com>
2023-03-14 19:26:00 +0900

    setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { SearchModule } from './search/search.module';
-
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [SearchModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.dev'],
+    }),
+    HttpModule,
+    SearchModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
